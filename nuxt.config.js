e9f8d635c4cc5302f59bf50a7a5d92f76f007480
Ryosuke Miyamoto <yahsan2@gmail.com>
2018-04-30 19:13:13 +1000

    FIxed api

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,8 +53,8 @@ module.exports = {
     interval: 1000,
     routes () {
       return Promise.all([
-        axios.get(`${apiUrl}/wp-json/wp/posts?per_page=100&page=1&_embed=1`),
-        axios.get(`${apiUrl}/wp-json/wp/pages?per_page=100&page=1&_embed=1`)
+        axios.get(`${apiUrl}/wp-json/wp/v2/posts?per_page=100&page=1&_embed=1`),
+        axios.get(`${apiUrl}/wp-json/wp/v2/pages?per_page=100&page=1&_embed=1`)
       ]).then((data) => {
         const posts = data[0]
         const pages = data[1]
